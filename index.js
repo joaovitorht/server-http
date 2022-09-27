75c74301ce483d74389ae473df686b23dce22ef7
joao <joao@gmail.com>
2022-09-27 17:12:54 -0400

    iniciando projeto

---
+++ index.js
@@ -0,0 +1,21 @@
+const http = require("http");
+
+const server = http.createServer((requisicao, resposta) => {
+
+  resposta.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+
+  if (requisicao.method === "GET") {
+    resposta.write("<h1>GET</h1>");
+  } else if (requisicao.method === "POST") {
+    resposta.write("<h1>POST</h1>");
+  } else if (requisicao.method === "PUT") {
+    resposta.write("<h1>PUT</h1>");
+  } else if (requisicao.method === "DELETE") {
+    resposta.write("<h1>DELETE</h1>");
+  }
+
+  resposta.end();
+});
+
+
+server.listen(3000, console.log("Servidor rodando...."));
