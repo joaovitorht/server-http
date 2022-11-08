ba54c49504e3f48b280c384faa9baffc977c0639
joao <joao@gmail.com>
2022-11-08 17:04:24 -0400

    first commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = http.createServer((requisicao, resposta) => {
   resposta.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
 
   if (requisicao.method === "GET") {
-    resposta.write("<h1>GET</h1>");
+    resposta.write("<h1>brasil</h1>");
   } else if (requisicao.method === "POST") {
     resposta.write("<h1>POST</h1>");
   } else if (requisicao.method === "PUT") {
